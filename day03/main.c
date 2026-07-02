b325ee6c99df31fa175a9c5fe052ec652a298066
Luke Davis <lukeda117@gmail.com>
2026-07-02 10:35:14 +0100

    Day 3 Part 2

--- day03/main.c
+++ day03/main.c
@@ -1,46 +1,79 @@
+#include <assert.h>
+#include <inttypes.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-int getLargestPossibleJoltage(char *bank) {
-  int bankLength = strlen(bank);
-  char firstLargestJoltage = bank[0];
-  int firstJoltagePosition = 0;
+#define JOLTAGE_DIGITS 12
 
-  for (int bankIndex = 0; bankIndex < bankLength - 1; bankIndex++) {
-    if (bank[bankIndex] - firstLargestJoltage > 0) {
-      firstLargestJoltage = bank[bankIndex];
-      firstJoltagePosition = bankIndex;
+typedef struct {
+    char joltage;
+    size_t bankIndex;
+} Battery;
+
+Battery getLargestBattery(char *batteryBank, size_t searchStartIndex, size_t searchEndIndex) {
+    assert(searchStartIndex <= searchEndIndex);
+
+    Battery largest;
+    largest.joltage = batteryBank[searchStartIndex];
+    largest.bankIndex = searchStartIndex;
+
+    for (size_t bankIndex = searchStartIndex; bankIndex <= searchEndIndex; bankIndex++) {
+        if (batteryBank[bankIndex] - largest.joltage > 0) {
+            largest.joltage = batteryBank[bankIndex];
+            largest.bankIndex = bankIndex;
+        }
     }
-  }
 
-  char secondLargestJoltage = bank[firstJoltagePosition + 1];
-  for (int bankIndex = firstJoltagePosition + 1; bankIndex < bankLength;
-       bankIndex++) {
-    if (bank[bankIndex] - secondLargestJoltage > 0) {
-      secondLargestJoltage = bank[bankIndex];
+    return largest;
+}
+
+uint64_t getLargestJoltage(char *batteryBank) {
+    size_t bankLength = strlen(batteryBank);
+    assert(bankLength >= JOLTAGE_DIGITS);
+
+    char bankJoltage[JOLTAGE_DIGITS + 1] = "";
+    int remainingJoltageDigits = JOLTAGE_DIGITS;
+    size_t searchStartIndex = 0;
+    size_t searchEndIndex;
+
+    while (remainingJoltageDigits > 0) {
+        remainingJoltageDigits -= 1;
+        searchEndIndex = bankLength - 1 - (size_t)remainingJoltageDigits;
+
+        Battery largestBattery = getLargestBattery(batteryBank, searchStartIndex, searchEndIndex);
+
+        char largestBatteryJoltage[2] = {largestBattery.joltage, '\0'};
+        strcat(bankJoltage, largestBatteryJoltage);
+        searchStartIndex = largestBattery.bankIndex + 1;
     }
-  }
 
-  char joltage[3];
-  sprintf(joltage, "%c%c", firstLargestJoltage, secondLargestJoltage);
-
-  return atoi(joltage);
+    return atoll(bankJoltage);
 }
 
 int main(int argc, char *argv[]) {
-  FILE *input;
-  input = fopen(argv[1], "r");
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <input-file>\n", argv[0]);
+        return 1;
+    }
 
-  char bank[102];
-  int totalOutputJoltage = 0;
+    FILE *input;
+    input = fopen(argv[1], "r");
+    if (input == NULL) {
+        fprintf(stderr, "File '%s' could not be opened\n", argv[1]);
+        return 1;
+    }
 
-  while (fgets(bank, sizeof(bank), input)) {
-    bank[strcspn(bank, "\n")] = 0;
-    totalOutputJoltage += getLargestPossibleJoltage(bank);
-  }
+    char bank[102];
+    uint64_t totalOutputJoltage = 0;
 
-  fclose(input);
+    while (fgets(bank, sizeof(bank), input)) {
+        bank[strcspn(bank, "\n")] = 0;
+        totalOutputJoltage += getLargestJoltage(bank);
+    }
 
-  printf("Total Joltage: %d\n", totalOutputJoltage);
+    fclose(input);
+
+    printf("Total Joltage: %" PRId64 "\n", totalOutputJoltage);
 }
